8d508cd466317ba4dffe666dd97cc8d49672815e
twmay006 <twmay006@163.com>
2018-03-06 09:51:33 +0800

    first commit

---
+++ config/index.js
@@ -0,0 +1,22 @@
+var path = require('path')
+
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubdirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: true,
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css']
+  },
+  dev: {
+    env: require('./dev.env'),
+    port: 3333,
+    assetsSubdirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+    cssSourceMap: false
+  }
+}
